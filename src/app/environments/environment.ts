3afe4d893087bc0cb2d06d4caf4e96e216ac93ba
Ayush <ayushdandekar00@gmail.com>
2025-11-27 13:31:00 +0530

    code env update

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:4000/api'
+  // apiBase: 'http://localhost:4000/api'
+  apiBase: ' https://procohat-backend.onrender.com/api'
 };
